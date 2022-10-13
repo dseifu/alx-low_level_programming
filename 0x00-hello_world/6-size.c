0e01f84f48e11f5a6b375d6b3c2b6b35c5d8f7eb
dseifu <dawitseifu@gmail.com>
2022-10-13 08:23:48 -0700

    a bit corr to 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 char c;
-int i;  
+int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of a char:%s byte(s)\n", sizeof(c));
-printf("Size of a int:%s byte(s)\n", sizeof(i));
-printf("Size of a long int:%s byte(s)\n", sizeof(li));
-printf("Size of a long long int:%s byte(s)\n", sizeof(lli));
-printf("Size of a float:%s byte(s)\n", sizeof(f));
+printf("Size of a char:%lu byte(s)\n", sizeof(c));
+printf("Size of a int:%lu byte(s)\n", sizeof(i));
+printf("Size of a long int:%lu byte(s)\n", sizeof(li));
+printf("Size of a long long int:%lu byte(s)\n", sizeof(lli));
+printf("Size of a float:%lu byte(s)\n", sizeof(f));
 }
