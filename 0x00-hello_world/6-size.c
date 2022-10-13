8e99525b96f0da6868b61881393c5d53ff2cbf6e
dseifu <dawitseifu@gmail.com>
2022-10-13 08:10:59 -0700

    done with 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - the entry point
+*
+* Return: 0 to exist
+*/
+int main(void)
+{
+long int i;
+long long int j;
+printf("Size of a char:%s byte(s)\n", sizeof(char));
+printf("Size of a int:%s byte(s)\n", sizeof(int));
+printf("Size of a long int:%s byte(s)\n", sizeof(i));
+printf("Size of a long long int:%s byte(s)\n", sizeof(j));
+printf("Size of a float:%s byte(s)\n", sizeof(float));
+}
