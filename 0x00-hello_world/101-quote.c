ffee4793c2980751c832bedfbcf690016693f3f2
dseifu <dawitseifu@gmail.com>
2022-10-13 11:55:29 -0700

    done with 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - the entry point
+*
+* Return: 1 to exit
+*/
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
