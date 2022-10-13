33e45ee30e4fb55d0f3f0dcbebe46df6fe57b848
dseifu <dawitseifu@gmail.com>
2022-10-13 10:22:10 -0700

    removing the last new line 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of a char:%lu byte(s)\n", sizeof(c));
 printf("Size of a int:%lu byte(s)\n", sizeof(i));
 printf("Size of a long int:%lu byte(s)\n", sizeof(li));
 printf("Size of a long long int:%lu byte(s)\n", sizeof(lli));
-printf("Size of a float:%lu byte(s)\n", sizeof(f));
+printf("Size of a float:%lu byte(s)", sizeof(f));
 return (0);
 }
