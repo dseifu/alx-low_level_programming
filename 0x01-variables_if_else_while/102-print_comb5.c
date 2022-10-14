de8c318961d7959a1db9feefda3fd4af78ca4bd8
dseifu <dawitseifu@gmail.com>
2022-10-14 10:51:23 -0700

    done with 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+* main - the entry point
+*
+* Return: 0 to exit
+*/
+int main(void)
+{
+int n1d1;
+int n1d2;
+int n2d1;
+int n2d2;
+for (n1d1 = 48; n1d1 <= 57; n1d1++)
+{
+for (n1d2 = 48; n1d2 <= 57; n1d2++)
+{
+for (n2d1 = n1d1; n2d1 <= 57; n2d1++)
+{
+for (n2d2 = n1d2; n2d2 <= 57; n2d2++)
+{
+if ((n1d1 == 48 && n1d2 == 48 && n2d1 == 48 && n2d2 == 48) || (n1d1 == 57 && n1d2 == 57 && n2d1 == 57 && n2d2 == 57))
+continue;
+putchar(n1d1);
+putchar(n1d2);
+putchar(' ');
+putchar(n2d1);
+putchar(n2d2);
+if (n1d1 == 57 && n2d1 == 57 && n2d2 == 57 && n1d2 == 56)
+{
+continue;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar(10);
+return (0);
+}
