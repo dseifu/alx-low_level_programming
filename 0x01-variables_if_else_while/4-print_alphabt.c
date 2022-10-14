d27c9fd29e81dde05439eadece9247c902dfc86e
dseifu <dawitseifu@gmail.com>
2022-10-14 03:37:47 -0700

    done wiht 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - the entry point
+*
+* Return: 0 to exit
+*/
+int main(void)
+{
+char alp[] = "abcdfghijklmnoprstuvwxyz";
+for (int i = 0; i < 24; i++)
+ putchar(alp[i]);
+return (0);
+}
