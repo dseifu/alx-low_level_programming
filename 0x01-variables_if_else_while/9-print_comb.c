d00d62b118f383ce0a7d0b5aa201e92d7200f3e7
dseifu <dawitseifu@gmail.com>
2022-10-14 06:01:09 -0700

    done with 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - the entry point
+*
+* Return: 0 to exit
+*/
+int main(void)
+{
+int i;
+for (i = 48; i <=57; i++)
+{
+putchar(i);
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
