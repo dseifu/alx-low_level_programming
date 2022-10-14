1119d3c293a82467b465543ab0181e9c40f677e4
dseifu <dawitseifu@gmail.com>
2022-10-14 07:08:03 -0700

    done with 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main
+*
+* Return: 0 to exit
+*/
+int main(void)
+{
+int i;
+int j;
+for(i = 48; i <= 57; i++)
+{
+for(j = i+1; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+if(i == 56 && j == 57)
+{
+continue;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar(10);
+return (0);
+}
