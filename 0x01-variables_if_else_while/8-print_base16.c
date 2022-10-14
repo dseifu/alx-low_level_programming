ae5c119f9d104c30f8bb4243adebc7ad0d04d52d
dseifu <dawitseifu@gmail.com>
2022-10-14 05:11:51 -0700

    done with 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - the entry point
+*
+* Return: 0 to exit
+*/
+int main(void)
+{
+char hex[] = "0123456789abcdef";
+for (int i = 0; i < 16; i++)
+putchar(hex[i]);
+return (0);
+}
