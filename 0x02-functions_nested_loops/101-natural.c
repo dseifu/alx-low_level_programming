b91175fa49fa39d8b1c0f87f2d32bd062408b386
dseifu <dawitseifu@gmail.com>
2022-10-18 16:32:28 -0700

    just to check if it works Q13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -16,9 +16,6 @@ int main(void)
 	{
 		mul3 = 3 * i;
 		mul5 = 5 * i;
-		if (mul3 == mul5 && mul5 < 1024)
-			sum = sum + mul3;
-		else
 		{
 			if (mul3 < 1024 && mul5 < 1024)
 				sum = sum + mul3 + mul5;
