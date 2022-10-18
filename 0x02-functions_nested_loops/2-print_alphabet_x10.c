e1e0e5b62440699ebc8e9423802be89427160c05
dseifu <dawitseifu@gmail.com>
2022-10-18 04:18:37 -0700

    done with 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print all letters in lower case 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+	int alp[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(alp[j]);
+		}
+		_putchar(10);
+	}
+}
