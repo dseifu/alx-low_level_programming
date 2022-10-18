57e3d8c1b2a05d9894f0174276815949a39d61f5
dseifu <dawitseifu@gmail.com>
2022-10-18 09:00:05 -0700

    considering edge case on 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,9 @@ int print_last_digit(int n)
 {
 	int lastdig;
 
-	if (n < 0)
-		n = n * -1;
 	lastdig = n % 10;
+	if (lastdig < 0)
+		lastdig = lastdig * -1;
 	_putchar(lastdig + '0');
 	return (lastdig);
 }
