3c1f41b226215eb2e33a0079dbe046d90bc8f825
dseifu <dawitseifu@gmail.com>
2022-10-18 04:35:28 -0700

    hopefully done with 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,17 @@ void print_alphabet_x10(void)
 {
 	int i;
 	int j;
-	char alp[] = "abcdefghijklmnopqrstuvwxyz'\n'";
+	char alp[] = "abcdefghijklmnopqrstuvwxyz";
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 26; j++)
+		if (i < 9)
 		{
-			_putchar(alp[j]);
+			for (j = 0; j < 26; j++)
+			{
+				_putchar(alp[j]);
+			}
 		}
+		_putchar(10);
 	}
-	_putchar(10);
 }
