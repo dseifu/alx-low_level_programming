a2f3e01a87292de9bcf9d354d11ce76dd9f54c4f
dseifu <dawitseifu@gmail.com>
2022-10-18 04:02:53 -0700

    done with 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+/**
+ * print_alphabet - print all letters, lowercase
+ */
+void print_alphabet(void)
+{
+	int i;
+	char alp[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alp[i]);
+	}
+}
