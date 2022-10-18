2134a7eca0618edb822dcc3796a9035a5511dbd3
dseifu <dawitseifu@gmail.com>
2022-10-18 03:53:07 -0700

    done with 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - the entry point
+ *
+ *Return: 0 to exit
+ */
+int main(void)
+{
+	int i;
+	char str[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+	return (0);
+}
