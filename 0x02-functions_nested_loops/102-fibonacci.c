1aa47b00534a3724df112b1794705d690414a9ae
dseifu <dawitseifu@gmail.com>
2022-10-18 17:12:55 -0700

    done with 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ *
+ * Return: 0 to exit
+ */
+int main(void)
+{
+	int i;
+	int fib[50];
+
+	fib[0] = 1;
+	fib[1] = 2;
+	for (i = 2; i < 50; i++)
+		fib[i] = fib[i - 2] + fib[i - 1];
+	for (i = 0; i < 50; i++)
+	{
+		printf("%i", fib[i]);
+		if (i != 50)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
