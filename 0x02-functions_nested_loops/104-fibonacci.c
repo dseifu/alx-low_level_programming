8f6df51f8b2ff1e90e0db198154bc4661e3b034d
dseifu <dawitseifu@gmail.com>
2022-10-18 18:17:57 -0700

    refreshing my knowledge on reursion for Q16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <main.h>
+/**
+ * fibo - calulate the nth Fibonacci number in Fibonacci numbers
+ * @n: The nth number in Fibonacci numbers
+ *
+ * Return: The value of the nth Fibonacci number
+ */
+long long fibo(int n)
+{
+	if (n == 1)
+		return (1);
+	if (n == 2)
+		return (2);
+	if (n > 2)
+		return (fibo(n - 2) + fibo(n - 1));
+}
+/**
+ * main - the entry point
+ *
+ * Return: 0 to exit
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 98; i++)
+	{
+		printf("%lli", fibo(i));
+		if (i != 98)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
