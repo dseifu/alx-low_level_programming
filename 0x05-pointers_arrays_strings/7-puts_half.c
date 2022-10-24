00604cfc6ccd4ce53fa73b0e64c4f553f54c2209
dseifu <dawitseifu@gmail.com>
2022-10-24 08:43:18 -0700

    done with 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - print half of a given string's lenght chars
+ * @str: the string
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int l = strlen(str);
+	int hl;
+
+	if (l % 2 == 0)
+		hl = l / 2;
+	else
+		hl = l - 1 / 2;
+	for (i = hl; i < l; i++)
+		_putchar(str[i]);
+	_putchar(10);
+}
