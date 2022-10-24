4d71779c48cbf322952e87717a4ad7ac9e3eab22
dseifu <dawitseifu@gmail.com>
2022-10-24 10:06:15 -0700

    removed white spaces on 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * _atoi - change character to integer
  * @s: the string to be changed
@@ -10,5 +10,13 @@
 
 int _atoi(char *s)
 {
+	unsigned long int i;
+	unsigned long int l = strlen(s);
+
+	for (i = 0; i < l; i++)
+	{
+		if (s[i] == ' ')
+			s[i] = '+';
+	}
 	return (atoi(s));
 }
