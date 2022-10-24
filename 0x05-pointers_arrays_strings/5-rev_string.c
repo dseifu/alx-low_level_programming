c0708b13b698082bae208138e84f718ecdcb4437
dseifu <dawitseifu@gmail.com>
2022-10-24 04:42:32 -0700

    a little corr to 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,6 +10,10 @@ void rev_string(char *s)
 {
 	int i;
 
+	for (i = 0; i < strlen(s); i++)
+		_putchar(s[i]);
+	_putchar(10);
+
 	for (i = strlen(s) - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar(10);
