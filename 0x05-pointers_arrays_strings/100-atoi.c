7e068185760ccc818c6f3e945ddf8829d0eeaf5c
dseifu <dawitseifu@gmail.com>
2022-10-24 09:41:29 -0700

    done with 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - change character to integer
+ * @s: the string to be changed
+ *
+ * Return: the changed int
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
