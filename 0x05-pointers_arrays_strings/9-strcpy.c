598324c63bf0567f444ac93db9a6470c502bbbdf
dseifu <dawitseifu@gmail.com>
2022-10-24 09:28:53 -0700

    done with 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy one string to another
+ * @dest: destination string to be copied into
+ * @src: the src string to copy from
+ *
+ * Return: a pointer to the copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (&dest);
+}
