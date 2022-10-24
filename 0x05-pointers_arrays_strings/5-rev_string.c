d4049251427627c3c7f9236b2a1a22e3873103cc
dseifu <dawitseifu@gmail.com>
2022-10-24 05:26:39 -0700

    trying builting fn on 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,17 @@
 
 void rev_string(char *s)
 {
-	int i;
-	char str[] = {s};
-	int index = 0;
+	/**
+	 * int i;
+	 * char str = s;
+	 * int index = 0;
 
-	for (i = strlen(s) - 1; i >= 0; i--)
-	{
-		str[index] = s[i];
-		index++;
-	}
-	s = str;
+	 * for (i = strlen(s) - 1; i >= 0; i--)
+	 * {
+	 * 	str[index] = s[i];
+	 * 	index++;
+	 * }
+	 * s = str;
+	 */
+	strrev(s);
 }
