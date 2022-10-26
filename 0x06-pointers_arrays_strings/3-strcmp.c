09570b7a64f3615034edabdb923941c8cd7aa30a
dseifu <dawitseifu@gmail.com>
2022-10-26 11:41:29 -0700

    done with 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare two strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: 0 if identical pos if greater and neg is less
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
