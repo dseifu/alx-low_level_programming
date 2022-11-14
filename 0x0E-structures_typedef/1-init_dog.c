1bf6b68a369ef2566de6865f5798b5271549cb8f
dseifu <dawitseifu@gmail.com>
2022-11-14 14:36:14 -0800

    done with Q1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: the struct
+ * @name: one element variable of the given struct
+ * @age: one element variable of the given struct
+ * @owner: one element variable of the given struct
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+
 #include "dog.h"
+
+
 
 /**
 
@@ -19,11 +22,7 @@ int main(void)
 	
 
 	
-	my_dog.name = "Poppy";
-	
-	my_dog.age = 3.5;
-	
-	my_dog.owner = "Bob";
+	init_dog(&my_dog, "Poppy", 3.5, "Bob");
 	
 	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 	
