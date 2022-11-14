34eed376bb6717811b1b6b68cc27c6e79507bf3c
dseifu <dawitseifu@gmail.com>
2022-11-14 15:21:35 -0800

    done with Q4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: one of the member variable of the new created dog strcut
+ * @age: one of the member variable of the new created dog strcut
+ * @owner: one of the member variable of the new created dog strcut
+ *
+ * Return: pointer
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = NULL;
+
+	d = malloc(sizeof(dog_t));
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	return (d);
+}
