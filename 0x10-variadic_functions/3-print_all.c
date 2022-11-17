f037883b0824bd7976aeba7e016728245bebee52
dseifu <dawitseifu@gmail.com>
2022-11-17 11:51:16 -0800

    done with Q3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,76 @@
+#include "variadic_functions.h"
+#include <string.h>
+
+/**
+ * switch_f_and_print - switch format chars one by one and print accordingly
+ * @f: format char
+ * @ap: va_list var 1
+ * @save: va_list var 2
+ *
+ * Return: 1 if f is a valid case 0 if not
+*/
+
+int switch_f_and_print(char f, va_list ap, va_list save)
+{
+	int is_valid = 0;
+
+	switch (f)
+	{
+	case 'c':
+		is_valid = 1;
+		printf("%c", va_arg(ap, int));
+		va_arg(save, int);
+		break;
+	case 'i':
+		is_valid = 1;
+		printf("%d", va_arg(ap, int));
+		va_arg(save, int);
+		break;
+	case 'f':
+		is_valid = 1;
+		printf("%f", va_arg(ap, double));
+		va_arg(save, double);
+		break;
+	case 's':
+		is_valid = 1;
+		if (va_arg(ap, char *) != NULL)
+		{
+			printf("%s", va_arg(save, char *));
+			break;
+		}
+		printf("(nil)");
+		va_arg(save, char *);
+		break;
+	}
+	return (is_valid);
+}
+
+/**
+ * print_all - print any num and typ of args
+ * @format: string used to determine num and type of args
+*/
+
+void print_all(const char * const format, ...)
+{
+	va_list ap, save;
+	unsigned int l;
+	unsigned int i = 0;
+	char f;
+
+	if (format != NULL)
+	{
+		l = strlen(format);
+		va_start(ap, format);
+		va_start(save, format);
+		while (i < l)
+		{
+			f = format[i];
+			if (switch_f_and_print(f, ap, save) && i != l - 1)
+				printf(", ");
+			i++;
+		}
+		printf("\n");
+		va_end(ap);
+		va_end(save);
+	}
+}
