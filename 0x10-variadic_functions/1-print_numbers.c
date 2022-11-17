4e5cd6f9e1f589a80a53f10813e2beefdf34a892
dseifu <dawitseifu@gmail.com>
2022-11-17 09:59:33 -0800

    removed what has been added earlier on Q1 and Q2 and print new line on all scenarios

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,7 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
-	if (separator != NULL && n > 0)
+	if (separator != NULL)
 	{
 		va_start(ap, n);
 		for (i = 0; i < n; i++)
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (i != n - 1)
 				printf("%s", separator);
 		}
-		printf("\n");
 		va_end(ap);
 	}
+	printf("\n");
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,7 +11,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap, save;
 
-	if (separator != NULL && n > 0)
+	if (separator != NULL)
 	{
 		va_start(ap, n);
 		va_start(save, n);
@@ -27,8 +27,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (i != n - 1)
 				printf("%s", separator);
 		}
-		printf("\n");
 		va_end(ap);
 		va_end(save);
 	}
+	printf("\n");
 }
