53eac51521d2a446f8b1c192add7396bf5d44ca3
dseifu <dawitseifu@gmail.com>
2022-11-29 14:14:40 -0800

    some mod to Q9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		else
 		{
-			while (ith_node != NULL)
+			while (ith_node != NULL && c < idx)
 			{
 				if (idx == c + 1)
 				{
