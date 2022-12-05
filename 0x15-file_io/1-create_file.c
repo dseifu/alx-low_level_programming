656e31a9cc0f079687bc4d5c4aae728aa1cfdc1f
dseifu <dawitseifu@gmail.com>
2022-12-05 11:13:01 -0800

    corr to Q1 and Q2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,33 @@
 #include "main.h"
 
 /**
- * create_file - creates a file. The file must have those permissions rw-------
- * If the file already exists, do not change the permissions
- * if the file already exists, truncate it
- * if text_content is NULL create an empty file
- * @filename: name of the file to create
- * @text_content: a NULL terminated string to write to the file
+ * create_file - creates a file and puts text in it
+ * with 600 perms (do not change if it exists)
+ *
+ * @filename: name for file
+ * @text_content: text to put into file
  *
  * Return: 1 on success, -1 on failure
- * if filename is NULL return -1
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	size_t len;
+	int file;
+	int length = 0, inlen = 0;
+	char *ptr;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
-	if (text_content)
+	if (text_content != NULL)
 	{
-		for (len = 0; !text_content[len]; len++)
-		{}
+		for (inlen = 0, ptr = text_content; *ptr; ptr++)
+			inlen++;
+		length = write(file, text_content, inlen);
 	}
-	if (len == 0)
-	{
-		close(fd);
-		return (1);
-	}
-	else if (write(fd, text_content, len) == -1)
-	{
-		close(fd);
+	if (close(file) == -1 || inlen != length)
 		return (-1);
-	}
-	close(fd);
 	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,33 +1,30 @@
 #include "main.h"
 
 /**
- * append_text_to_file - function that appends text at the end of a file
- * @filename: Name of the file
- * @text_content: String to write
+ * append_text_to_file - creates a file and puts text in it
+ * with 600 perms (do not change if it exists)
+ *
+ * @filename: name for file
+ * @text_content: text to put into file
  *
  * Return: 1 on success, -1 on failure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	size_t len;
+	int file;
+	ssize_t length, inlen;
+	char *ptr;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
 		return (-1);
-	if (text_content)
-	{
-		for (len = 0; text_content[len]; len++)
-		{}
-	}
-	if (write(fd, text_content, len) == -1)
-	{
-		close(fd);
+	for (inlen = 0, ptr = text_content; *ptr; ptr++)
+		inlen++;
+	length = write(file, text_content, inlen);
+	if (close(file) == -1 || inlen != length)
 		return (-1);
-	}
-	close(fd);
-	return (1);
+	return (length);
 }
